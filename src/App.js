aaf688880f1c8ce09a014acbaa4875962a816045
mmax2001 <maxi2001@gmail.com>
2022-01-26 22:59:00 -0300

    corrijo grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import './styles.css';
 import { ItemListContainer } from "./components/container/ItemListContainer";
-import { Component } from 'react';
 import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
@@ -10,8 +9,7 @@ function App() {
         <
         div className = 'contenedorGrid' >
         <
-        div className = ''
-        header > < NavBar > < /NavBar></div >
+        div className = 'header' > < NavBar > < /NavBar></div >
         <
         div className = 'mainPersonal' > < ItemListContainer greeting = "Hola Coders!" / > < /div> {
             /* <NavBar className='header'></NavBar>
