82b1d857b6043a8fafc7fc0dc5f70607e6b24724
mmax2001 <maxi2001@gmail.com>
2022-02-13 19:33:35 -0300

    arreglos en vista detallada de item

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,15 +1,27 @@
-import { Button } from "react-bootstrap"
+import { Button,Card } from "react-bootstrap"
+import './ItemDetail.css'
 
 export const ItemDetail=({nombre,precio,id,categoria,imagen,descrip,cantidad,stock})=>{
     return (
-        <div style={{marginLeft:"20px"}}>
-            <h3>{nombre}</h3>
+        <div style={{justifyContent:"center"}}>
+            {/* <h3>{nombre}</h3>
             <img src={imagen} alt={nombre} />
             <p>{descrip}</p>
-            <h5>Precio ${precio}</h5>
-            <Button>+</Button>
-            <span> Modificar cantidad </span>
-            <Button>-</Button>
+            <h5>Precio ${precio}</h5> */}
+            <Card style={{ width: '35rem'}} className="cardStyle">
+            <Card.Img variant="top" src={imagen} />
+            <Card.Body>
+                <Card.Title>{nombre}</Card.Title>
+                <Card.Text>
+                    <span>Precio : ${precio}</span>
+                    <p>Cantidad</p>                    
+                </Card.Text>
+                <Card.Text>{descrip}</Card.Text>
+                <Button>+</Button>
+                <span> Modificar cantidad </span>
+                <Button>-</Button>              
+            </Card.Body>
+            </Card>
         </div>
     )
 }
